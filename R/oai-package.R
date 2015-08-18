288296308bec59b23bb5799b707426a11f754e4b
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-18 09:42:55 -0700

    importfrom for non-base R pkgs, bump patch ver, fix #4

--- R/oai-package.R
+++ R/oai-package.R
@@ -2,6 +2,9 @@
 #'
 #' @name oai-package
 #' @aliases oai
+#' @importFrom methods is
+#' @importFrom stats setNames
+#' @importFrom utils head
 #' @importFrom httr GET content stop_for_status
 #' @importFrom xml2 read_xml xml_children xml_text as_list xml_attrs xml_name xml_attr
 #' @docType package
