59b63cc7bfdad3b189a0a8f6531a285a974fc2db
Michal Bojanowski <michal2992@gmail.com>
2015-09-25 10:35:04 +0200

    Alternative handle_errors2

--- R/list_records.R
+++ R/list_records.R
@@ -72,6 +72,19 @@ handle_errors <- function(x) {
   }
 }
 
+# Look for OAI-PMH exceptions
+# https://www.openarchives.org/OAI/openarchivesprotocol.html#ErrorConditions
+# xml = parsed xml
+handle_errors2 <- function(xml) {
+  nodeset <- xml2::xml_find_all(xml, ".//*[local-name()='error']")
+  if( length(nodeset) > 0 ) {
+    msgs <- sapply(nodeset, function(n)
+      paste0( xml2::xml_attr(n, "code"), ": ", xml2::xml_text(n) ) )
+    stop( paste0("OAI-PMH exceptions: ", paste(msgs, collapse="\n")) )
+  }
+
+}
+
 #' @export
 print.oai_df <- function(x, ..., n = 10) {
   cat(sprintf("<%s> %s X %s", attr(x, "type"), NROW(x), NCOL(x)), "\n\n")
