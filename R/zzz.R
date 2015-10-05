da2826188e0a2b06924be3ad9270582606268eb9
Michal Bojanowski <michal2992@gmail.com>
2015-10-05 10:47:53 +0200

    Even better OAI-PMH error handling
    Properly store error codes and messages, which will allow, potentially, to handle them somewhere else.

--- R/zzz.R
+++ R/zzz.R
@@ -11,3 +11,20 @@ pluck <- function(x, name, type) {
 last <- function(x) x[length(x)][[1]]
 
 datacite_base <- function() "http://oai.datacite.org/oai"
+
+
+
+
+
+# Simple condition generator as shown here
+# http://adv-r.had.co.nz/Exceptions-Debugging.html#condition-handling
+condition <- function(subclass, message, call = sys.call(-1), ...) {
+  structure(
+    class = unique(c(subclass, "condition")),
+    list(message = message, call = call),
+    ...
+  )
+}
+
+# is it a condition?
+is.condition <- function(x) inherits(x, "condition")
