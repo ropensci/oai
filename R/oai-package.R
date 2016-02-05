21a8e2ea856a5469ed6f774da1437a621d93e9ee
Scott Chamberlain <myrmecocystus@gmail.com>
2016-02-05 13:16:57 -0800

    added misising stringr import

--- R/oai-package.R
+++ R/oai-package.R
@@ -34,6 +34,7 @@
 #' @importFrom httr GET content stop_for_status
 #' @importFrom xml2 read_xml xml_children xml_text as_list xml_attrs xml_name xml_attr
 #' @importFrom plyr rbind.fill
+#' @importFrom stringr str_extract
 #' @docType package
 #' @title OAI-PMH Client
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
