4072aaa835001b4b85637be600da68f9eb65ac3c
Michal Bojanowski <michal2992@gmail.com>
2016-08-10 17:01:09 +0200

    Added oai_available() and check_oai()

---
+++ R/oai_available.R
@@ -0,0 +1,36 @@
+#' Test of OAI-PMH service is available
+#'
+#' Silently test if OAI-PMH service is available under the URL provided.
+#'
+#' @param u base URL to OAI-PMH service
+#' @param ... other arguments passed to \code{\link{id}}
+#'
+#' @return \code{TRUE} or \code{FALSE} if the service is available.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' url_list <- list(
+#'   archivesic="http://archivesic.ccsd.cnrs.fr/oai/oai.php",
+#'   datacite = "http://oai.datacite.org/oai",
+#'
+#'   # No OAI-PMH here
+#'   google = "http://google.com"
+#' )
+#'
+#' sapply(url_list, oai_available)
+#' }
+oai_available <- function( u, ... ) {
+  # Identify yourself!
+  r <- try( id(u, ...), silent=TRUE )
+  if( inherits(r, "try-error") ) return(FALSE)
+  else return(TRUE)
+}
+
+
+# Skip a testthat test if the service is not available
+check_oai <- function(u, ...) {
+  if( !oai_available(u, ...) ) {
+    testthat::skip( paste("OAI-PMH service at", u, "is not avaiable") )
+  }
+}
