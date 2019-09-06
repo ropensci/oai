9b3849c1363935df450ae38e8f14ad69140bf078
Scott Chamberlain <myrmecocystus@gmail.com>
2019-09-06 11:01:41 -0700

    changed template for url param

--- man-roxygen/url_ddd.R
+++ man-roxygen/url_ddd.R
@@ -1,2 +1,4 @@
-#' @param url (character) OAI-PMH base url
+#' @param url (character) OAI-PMH base url. Defaults to the URL for 
+#' arXiv's OAI-PMH server (http://export.arxiv.org/oai2)
+#' or GBIF's OAI-PMH server (http://api.gbif.org/v1/oai-pmh/registry)
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
